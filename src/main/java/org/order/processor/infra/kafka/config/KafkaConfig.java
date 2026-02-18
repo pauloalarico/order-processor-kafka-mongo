896eaa53c3981340f04c8c156351cc6f95d3f6e8
pauloalarico <paulocraft20@gmail.com>
2026-02-18 01:21:47 -0300

    fix - change the variables of the environment for the broker of the kafka, and the order

--- src/main/java/org/order/processor/infra/kafka/config/KafkaConfig.java
+++ src/main/java/org/order/processor/infra/kafka/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package org.order.processor.infra.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.order.processor.application.dto.request.CreateOrderDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +18,10 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String server;
+    @Value("${apps.order-path}")
+    private String createOrderPath;
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
@@ -27,14 +30,17 @@ public class KafkaConfig {
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(JacksonJsonDeserializer.TYPE_MAPPINGS, createOrderPath);
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         config.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "*");
+        config.put(JacksonJsonDeserializer.VALUE_DEFAULT_TYPE, CreateOrderDTO.class);
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> factory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
         var factory = new ConcurrentKafkaListenerContainerFactory<String,Object>();
+        factory.setConsumerFactory(consumerFactory);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
         factory.setConcurrency(3);
         return factory;
