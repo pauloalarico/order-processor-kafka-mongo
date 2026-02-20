661b84b920f3f63cc31809f17939db538f45ef66
pauloalarico <paulocraft20@gmail.com>
2026-02-20 03:27:50 -0300

    refactor - change the consumer for better pracs

--- src/main/java/org/order/processor/infra/kafka/consumer/KafkaConsumer.java
+++ src/main/java/org/order/processor/infra/kafka/consumer/KafkaConsumer.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.order.processor.application.dto.request.CreateOrderDTO;
 import org.order.processor.application.usecase.CalculateTotalUseCase;
+import org.order.processor.domain.model.OrderProcessor;
+import org.order.processor.infra.kafka.producer.OrderProcessProducer;
+import org.order.processor.infra.mapper.OrderProcessMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -14,13 +17,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaConsumer {
    private final CalculateTotalUseCase calculate;
+   private final OrderProcessProducer producer;
+   private final OrderProcessMapper mapper;
 
-    @KafkaListener(topics = "order-topic", groupId = "order-process")
+    @KafkaListener(topics = "${apps.kafka-listener}")
     public void consume(ConsumerRecord<String, CreateOrderDTO> consumerRecord, Acknowledgment acknowledgment) {
         try {
-            calculate.calculateAndSaveTotal(consumerRecord.value());
+            OrderProcessor orderProcessor = calculate.calculateAndSaveTotal(consumerRecord.value());
+            var dto = mapper.toResponseDTO(orderProcessor);
+            producer.send(dto);
             acknowledgment.acknowledge();
             log.info("Request sent with success, for correlation id {}, orderId {}", consumerRecord.value().correlationId(), consumerRecord.value().orderId());
+
         } catch (Exception e) {
          log.warn("Error processing the message, correlationId: {}", consumerRecord.value().correlationId());
         }
