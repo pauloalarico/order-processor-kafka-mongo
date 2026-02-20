faaf98e7fea91f74af61fefe240eb39c24dd3c33
pauloalarico <paulocraft20@gmail.com>
2026-02-20 03:25:51 -0300

    refactor - change the fields to final and add the status enum

--- src/main/java/org/order/processor/domain/model/OrderProcessor.java
+++ src/main/java/org/order/processor/domain/model/OrderProcessor.java
@@ -1,7 +1,8 @@
-package org.order.processor.domain.entitie;
+package org.order.processor.domain.model;
 
 import lombok.Getter;
 import org.order.processor.application.dto.request.CreateOrderDTO;
+import org.order.processor.domain.enums.Status;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,16 +13,22 @@ import java.math.BigDecimal;
 public class OrderProcessor {
     @Id
     private String correlationId;
-    private String product;
-    private BigDecimal totalValue;
+    private final String product;
+    private final BigDecimal totalValue;
+    private Status status;
 
     public OrderProcessor(CreateOrderDTO dto) {
         this.correlationId = dto.correlationId();
         this.product = dto.product();
         this.totalValue = calculateTotalValue(dto.price(), dto.quantity());
+        this.status = Status.ACTIVE;
     }
 
     private BigDecimal calculateTotalValue(BigDecimal amount, Integer quantity) {
         return amount.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    public void cancelValue() {
+        this.status = Status.CANCELED;
     }
 }
